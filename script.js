fc02fc76b63822c5f0aa44b6fdd0e7aefd139c57
princaryan57-dot <princaryan57@gmail.com>
2026-08-05 15:48:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function checkPassword() {
+function checkPassword(){
+
     let password = document.getElementById("pass").value;
 
-    if (password === "RIDBY2026") {
+    if(password === "RIDBY2026"){
+
         document.getElementById("downloadBox").style.display = "block";
+
         document.getElementById("message").innerHTML = "✅ Password Correct";
-    } else {
-        document.getElementById("downloadBox").style.display = "none";
-        document.getElementById("message").innerHTML = "❌ Wrong Password";
+
     }
+
+    else{
+
+        document.getElementById("message").innerHTML = "❌ Wrong Password";
+
+    }
+
 }
